c11cfa547fe470545fe4132f7a3c717b7f283b53
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-10-08 17:32:19 +0200

    Updates

--- source/uim/cake/Database/typeInterface.d
+++ source/uim/cake/Database/typeInterface.d
@@ -1,13 +1,4 @@
-
-
-/**
-
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         3.2.14
- * @license       https://opensource.org/licenses/mit-license.php MIT License
- */module uim.cake.databases;
+module uim.cake.databases;
 
 /**
  * Encapsulates all conversion functions for values coming from a database into PHP and
