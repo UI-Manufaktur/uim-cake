5a0938ac6ab5f99ba97817c3d0b117d918cd23c8
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-10-28 07:20:11 -0400

    Change datatypes

--- source/uim/cake/ORM/Association/Loader/SelectLoader.d
+++ source/uim/cake/ORM/Association/Loader/SelectLoader.d
@@ -1,13 +1,4 @@
-
-
-/**
-
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         3.4.0
- * @license       https://opensource.org/licenses/mit-license.php MIT License
- */module uim.cake.ORM\Association\Loader;
+module uim.cake.ORM\Association\Loader;
 
 import uim.cake.databases.Expression\IdentifierExpression;
 import uim.cake.databases.Expression\TupleComparison;
