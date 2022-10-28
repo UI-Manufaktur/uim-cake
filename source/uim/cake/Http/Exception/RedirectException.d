7469fc82f037ac9f94b827aa6439052b6ee3dca7
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-10-28 07:30:54 -0400

    Change datatypes

--- source/uim/cake/Http/Exception/RedirectException.d
+++ source/uim/cake/Http/Exception/RedirectException.d
@@ -1,13 +1,4 @@
-
-
-/**
-
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         4.1.0
- * @license       https://opensource.org/licenses/mit-license.php MIT License
- */module uim.cake.Http\Exception;
+module uim.cake.Http\Exception;
 
 /**
  * An exception subclass used by routing and application code to
