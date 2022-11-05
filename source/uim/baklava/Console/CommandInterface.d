8c37a079b1c52d9fbd5a482afa76315cc1d7fdbf
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-11-05 14:02:42 +0100

    Updates

---
+++ source/uim/baklava/Console/CommandInterface.d
@@ -0,0 +1,44 @@
+module uim.baklava.console;
+
+/**
+ * Describe the interface between a command
+ * and the surrounding console libraries.
+ */
+interface ICommand
+{
+    /**
+     * Default error code
+     *
+     * @var int
+     */
+    public const CODE_ERROR = 1;
+
+    /**
+     * Default success code
+     *
+     * @var int
+     */
+    public const CODE_SUCCESS = 0;
+
+    /**
+     * Set the name this command uses in the collection.
+     *
+     * Generally invoked by the CommandCollection when the command is added.
+     * Required to have at least one space in the name so that the root
+     * command can be calculated.
+     *
+     * @param string myName The name the command uses in the collection.
+     * @return this
+     * @throws \InvalidArgumentException
+     */
+    auto setName(string myName);
+
+    /**
+     * Run the command.
+     *
+     * @param array $argv Arguments from the CLI environment.
+     * @param \Cake\Console\ConsoleIo $io The console io
+     * @return int|null Exit code or null for success.
+     */
+    function run(array $argv, ConsoleIo $io): Nullable!int;
+}
---
+++ source/uim/baklava/Console/Exception/ConsoleException.php
@@ -0,0 +1,18 @@
+module uim.baklava.console\Exception;
+
+import uim.baklava.console.commandInterface;
+import uim.baklava.core.Exception\CakeException;
+
+/**
+ * Exception class for Console libraries. This exception will be thrown from Console library
+ * classes when they encounter an error.
+ */
+class ConsoleException : CakeException
+{
+    /**
+     * Default exception code
+     *
+     * @var int
+     */
+    protected $_defaultCode = ICommand::CODE_ERROR;
+}
