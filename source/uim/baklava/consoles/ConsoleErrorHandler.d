92376535a88e49327413905d5fc14aecd9b8a5d4
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-11-06 09:57:30 +0100

    Rename path

--- source/uim/baklava/consoles/ConsoleErrorHandler.d
+++
@@ -1,9 +0,0 @@
-
-
-class_alias(
-    'Cake\Error\ConsoleErrorHandler',
-    'Cake\Console\ConsoleErrorHandler'
-);
-deprecationWarning(
-    'import uim.baklava.errors\ConsoleErrorHandler instead of Cake\Console\ConsoleErrorHandler.'
-);
