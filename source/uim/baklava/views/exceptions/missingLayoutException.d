92c502383e8c76ef2d8e3fd9cf1705548ac8ddd7
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-11-06 10:21:32 +0100

    Rename Path

--- source/uim/baklava/views/exceptions/missingLayoutException.d
+++
@@ -1,8 +0,0 @@
-module uim.cakeews.exceptions;
-
-/**
- * Used when a layout file cannot be found.
- */
-class MissingLayoutException : MissingTemplateException {
-    protected string myType = 'Layout';
-}
