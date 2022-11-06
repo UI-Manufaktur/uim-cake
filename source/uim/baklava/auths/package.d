543eb09b7c6357fbd48b1814c2bcd9243926e0dd
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-11-06 09:56:20 +0100

    Rename path

--- source/uim/baklava/auths/package.d
+++
@@ -1,6 +0,0 @@
-module uim.baklava.auths;
-
-public import uim.baklava.auths.authenticates;
-public import uim.baklava.auths.authorizes;
-public import uim.baklava.auths.passwordhashers;
-public import uim.baklava.auths.storages;
