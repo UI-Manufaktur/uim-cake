abcf8e53b3acb55833c9dd3ec090670c91d1ecf5
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-11-12 18:23:15 +0100

    Namespace and file renaming

---
+++ source/uim/cake/https/clients/Exception/MissingResponseException.d
@@ -0,0 +1,14 @@
+module uim.cake.https.clients\Exception;
+
+import uim.cake.core.exceptions\CakeException;
+
+/**
+ * Used to indicate that a request did not have a matching mock response.
+ */
+class MissingResponseException : CakeException
+{
+    /**
+     * @var string
+     */
+    protected $_messageTemplate = 'Unable to find a mocked response for `%s` to `%s`.';
+}
