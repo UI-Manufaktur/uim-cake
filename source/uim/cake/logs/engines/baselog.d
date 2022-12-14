206103e5fbb3da9c8d7f17febc2924c1d818be89
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-12-14 18:02:06 +0100

    Convert to D

--- source/uim/cake/logs/engines/baselog.d
+++ source/uim/cake/logs/engines/baselog.d
@@ -1,14 +1,9 @@
-
-
-/**
- * UIM(tm) :  Rapid Development Framework (https://UIM.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakefoundation.org UIM(tm) Project
- * @since         2.2.0
- * @license       https://opensource.org/licenses/mit-license.php MIT License
- */module uim.cake.logs.engines;
+/*********************************************************************************************************
+*	Copyright: © 2015-2023 Ozan Nurettin Süel (Sicherheitsschmiede)                                        *
+*	License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  *
+*	Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      *
+**********************************************************************************************************/
+module uim.cake.logs.engines;
 
 use ArrayObject;
 import uim.cake.core.InstanceConfigTrait;
---
+++ source/uim/cake/logs/engines/package.d
@@ -0,0 +1,7 @@
+module uim.cake.logs.engines;
+
+public import uim.cake.logs.engines.arraylog;
+public import uim.cake.logs.engines.baselog;
+public import uim.cake.logs.engines.consolelog;
+public import uim.cake.logs.engines.filelog;
+public import uim.cake.logs.engines.syslog;
