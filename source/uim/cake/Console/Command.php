e9e8997e1dc7d67874580b3dfd23b305aeb8440a
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-12-24 08:22:26 +0100

    Convert to D

--- source/uim/cake/Console/Command.php
+++ source/uim/cake/Console/Command.php
@@ -1,5 +1,4 @@
-<?php
-declare(strict_types=1);
+
 
 /**
  * @deprecated 4.0.0 Use {@link \Cake\Command\Command} instead.
--- source/uim/cake/Console/ConsoleErrorHandler.php
+++ source/uim/cake/Console/ConsoleErrorHandler.php
@@ -1,5 +1,4 @@
-<?php
-declare(strict_types=1);
+
 
 class_exists('Cake\Error\ConsoleErrorHandler');
 deprecationWarning(
