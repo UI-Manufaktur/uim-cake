6477005a0cf3800ab5430d98fcd689174f2a398b
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-12-26 17:01:44 +0100

    Convert to D

--- source/uim/cake/Http/Client/Adapter/Curl.php
+++ source/uim/cake/Http/Client/Adapter/Curl.php
@@ -8,7 +8,7 @@
  * Redistributions of files must retain the above copyright notice.
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
+
  * @since         3.7.0
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
