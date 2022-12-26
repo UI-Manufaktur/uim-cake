08eb18a47ffe53afd00b02012602c911a0a883f3
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-12-26 16:49:25 +0100

    Convert to D

--- source/uim/cake/Utility/Hash.php
+++ source/uim/cake/Utility/Hash.php
@@ -1,12 +1,6 @@
 
 
-/**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
+
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
  * @since         2.2.0
