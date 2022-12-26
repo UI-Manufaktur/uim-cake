bac0be6752b804743024729ba3b12bf081bd0736
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-12-26 17:01:02 +0100

    Convert to D

--- source/uim/cake/Collection/Iterator/ZipIterator.php
+++ source/uim/cake/Collection/Iterator/ZipIterator.php
@@ -3,7 +3,7 @@
 
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
+
  * @since         3.0.5
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
--- source/uim/cake/Console/CommandFactory.php
+++ source/uim/cake/Console/CommandFactory.php
@@ -8,7 +8,7 @@
  * Redistributions of files must retain the above copyright notice.
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
+
  * @license       https://www.opensource.org/licenses/mit-license.php MIT License
  */
 module uim.cake.Console;
