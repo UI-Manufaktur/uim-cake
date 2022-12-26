712769d63f3ece53db8b23ad521c8db26fe25832
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-12-26 17:01:50 +0100

    Convert to D

--- source/uim/cake/I18n/PackageLocator.php
+++ source/uim/cake/I18n/PackageLocator.php
@@ -4,7 +4,7 @@
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
  * @copyright     Copyright (c) 2017 Aura for PHP
- * @link          https://cakephp.org CakePHP(tm) Project
+
  * @since         4.2.0
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
--- source/uim/cake/Log/Log.php
+++ source/uim/cake/Log/Log.php
@@ -8,7 +8,7 @@
  * Redistributions of files must retain the above copyright notice.
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
+
  * @since         0.2.9
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
