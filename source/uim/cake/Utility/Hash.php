d7f2ac3b9f655732a40e050171fdb0fd1549f195
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-12-29 08:23:59 +0100

    Convert to D

--- source/uim/cake/Utility/Hash.php
+++ source/uim/cake/Utility/Hash.php
@@ -2,7 +2,7 @@
 
 
  *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+
  * @since         2.2.0
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
