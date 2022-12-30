fc734810aa9dde975d97d0d03515643268a01f29
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-12-30 09:17:14 +0100

    Convert to D

--- source/uim/cake/TestSuite/Constraint/Response/BodyNotEquals.php
+++ source/uim/cake/TestSuite/Constraint/Response/BodyNotEquals.php
@@ -1,17 +1,4 @@
-
-
-/**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice
- *
-
- * @since         3.7.0
-
- */module uim.cake.TestSuite\Constraint\Response;
+module uim.cake.TestSuite\Constraint\Response;
 
 /**
  * BodyNotEquals
