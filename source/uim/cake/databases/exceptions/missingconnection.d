d8e02a32b18ddf71a292af2bcd0a50c9a113be70
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-01-11 14:39:25 +0100

    Convert to D

--- source/uim/cake/databases/exceptions/missingconnection.d
+++
@@ -1,9 +0,0 @@
-module uim.cake.databases.exceptions.missingconnection;
-
-@safe:
-import uim.cake;
-
-// Class MissingConnectionException
-class MissingConnectionException : UIMException {
-  protected string _messageTemplate = "Connection to %s could not be established: %s";
-}
