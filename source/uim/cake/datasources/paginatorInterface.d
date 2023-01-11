a3ff1ebe369515b261f01a8201462176b19a079e
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-01-11 09:16:15 +0100

    Convert to D

--- source/uim/cake/datasources/paginatorInterface.d
+++ source/uim/cake/datasources/paginatorInterface.d
@@ -5,26 +5,3 @@
 **********************************************************************************************************/
 module uim.datasources;
 
-// This interface describes the methods for paginator instance.
-interface IPaginator {
-    /**
-     * Handles pagination of datasource records.
-     *
-     * @param uim.cake.Datasource\IRepository|uim.cake.Datasource\IQuery $object The repository or query
-     *   to paginate.
-     * @param array myParams Request params
-     * @param array $settings The settings/configuration used for pagination.
-     * @return uim.cake.Datasource\IResultSet Query results
-     */
-    function paginate(object $object, array myParams = null, array $settings = null): IResultSet;
-
-    /**
-     * Get paging params after pagination operation.
-     * @return array
-     */
-    array getPagingParams();
-}
-class_exists("Cake\Datasource\Paging\PaginatorInterface");
-deprecationWarning(
-    "Use Cake\Datasource\Paging\PaginatorInterface instead of Cake\Datasource\PaginatorInterface."
-);
